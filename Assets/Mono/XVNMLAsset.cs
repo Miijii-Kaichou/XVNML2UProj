db975de8c3074cefb255cf2dea1bc704801eb36d
Tyrease Teer <kayaneseera@gmail.com>
2023-02-27 01:32:33 -0700

    File/Folder Restructuring
    Moving files around so that deployment of Unity Package will include everything that's needed.

---
+++ Assets/Mono/XVNMLAsset.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using XVNML.XVNMLUtility;
+
+[CreateAssetMenu(fileName = "New XVNML Asset", menuName = "XVNML/XVNML Asset")]
+public sealed class XVNMLAsset : ScriptableObject
+{
+    /// <summary>
+    /// Instance ID of the XVNML Asset
+    /// </summary>
+    public int InstanceID
+    {
+        get
+        {
+            if (m_InstanceID == 0)
+                m_InstanceID = GetInstanceID();
+
+            return m_InstanceID;
+        }
+    }
+    private int m_InstanceID;
+
+    /// <summary>
+    /// HashCode based on the name of the asset.
+    /// </summary>
+    public int hashCode;
+
+    /// <summary>
+    /// Original file path of this asset
+    /// </summary>
+    public string filePath;
+
+    public Object asset;
+
+    public XVNMLObj root;
+
+    public string content;
+}
